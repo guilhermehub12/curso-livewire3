91949763e9f6f3f335a2ebbede1ee91cb19cf1db
Guilherme Delmiro <guilhermedelmiro11@gmail.com>
2023-12-22 11:41:49 -0300

    Aula 4: Validação usando Rule

--- app/Livewire/Clicker.php
+++ app/Livewire/Clicker.php
@@ -4,20 +4,22 @@ namespace App\Livewire;
 
 use App\Models\User;
 use Illuminate\Http\Client\Request;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Clicker extends Component
 {
-    // public function handleClick()
-    // {
-    //     dump("Hello");
-    // }
+    #[Rule("required|min:2|max:50")]
     public $name;
+    #[Rule("required|email|unique:users")]
     public $email;
+    #[Rule("required|min:5")]
     public $password;
 
     public function createNewUser()
     {
+        $this->validate();
+
         User::create([
             'name' => $this->name,
             'email' => $this->email,
--- resources/views/livewire/clicker.blade.php
+++ resources/views/livewire/clicker.blade.php
@@ -1,10 +1,28 @@
 <div>
-    <form wire:submit='createNewUser' action="">
-        <input wire:model='name' type="text" placeholder="name">
-        <input wire:model='email' type="email" placeholder="email">
-        <input wire:model='password' type="password" placeholder="password">
+    <form class="p-5" wire:submit='createNewUser' action="">
+        <input class="block rounded border border-gray-100 px-3 py-1 mt-1" wire:model='name' type="text"
+            placeholder="name">
 
-        <button wire:click.prevent="createNewUser">Create</button>
+        @error('name')
+            <span class="text-red-500 text-xs">{{ $message }}</span>
+        @enderror
+
+        <input class="block rounded border border-gray-100 px-3 py-1 mt-1" wire:model='email' type="email"
+            placeholder="email">
+
+        @error('name')
+            <span class="text-red-500 text-xs">{{ $message }}</span>
+        @enderror
+
+        <input class="block rounded border border-gray-100 px-3 py-1 mt-1" wire:model='password' type="password"
+            placeholder="password">
+
+        @error('name')
+            <span class="text-red-500 text-xs">{{ $message }}</span>
+        @enderror
+
+        <button class="block rounded px-3 py-1 bg-gray-400 text-white"
+            wire:click.prevent="createNewUser">Create</button>
     </form>
 
     <hr>
